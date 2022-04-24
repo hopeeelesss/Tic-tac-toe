908cc4efce6b379fb8fe3ce788ffab7422a009a8
dmitry_chukov <dima.chukov.02@mail.ru>
2022-04-24 20:54:52 +0300

    Game's core is done + console game interface as example created.

---
+++ core/Player.java
@@ -0,0 +1,11 @@
+package core;
+
+import javax.management.InstanceAlreadyExistsException;
+
+public interface Player {
+    String getName();
+    void setName(String name);
+    void makeStep(int cellNumber) throws InstanceAlreadyExistsException;
+    Integer getFigureInt();
+    String getFigureStr();
+}
