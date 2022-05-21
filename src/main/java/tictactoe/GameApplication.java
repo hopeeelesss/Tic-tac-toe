467a33469b3688b634c23a7bedcc52974bc2e133
Victor <khokhlov34@gmail.com>
2022-05-21 22:11:45 +0300

    Зафиксировал рамер окна

--- src/main/java/tictactoe/GameApplication.java
+++ src/main/java/tictactoe/GameApplication.java
@@ -11,7 +11,7 @@ public class GameApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(GameApplication.class.getResource("menu.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("Tic-tac-toe");
         stage.setScene(scene);
         stage.show();
